a651a7072f635ae8093956e02da7db249afc09b0
Zachary Lincoln <zach@daftlabs.com>
2017-08-20 17:29:07 -0400

    many desktop styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $('.navbar-toggle').on('click', function () {
     $(this).toggleClass('collapsed');
 });
+
+$('.carousel').on('slide.bs.carousel', function (e) {
+    var nextH = $(e.relatedTarget).height();
+    $(this).find('.active.item').parent().animate({
+        height: nextH
+    }, 500);
+});
